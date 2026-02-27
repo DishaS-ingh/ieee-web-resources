34b8df81194c31d5416f4635082eca8ebadf1fb5
DishaS-ingh <dishasingh2714@gmail.com>
2026-02-27 17:13:30 +0530

    Added image placeholder to Team Card

--- frontend/src/TeamCard.jsx
+++ frontend/src/TeamCard.jsx
@@ -1,8 +1,14 @@
+import placeholder from "./assets/placeholder.png";
 function TeamCard({ name, chapter, role, description }) {
   return (
     <div className="group relative bg-gradient-to-br from-[#0f1b3d] to-[#0a0f2c] rounded-xl p-6 text-white overflow-hidden transform hover:scale-105 transition duration-300 shadow-lg">
 
       <div className="group-hover:opacity-0 transition duration-300">
+        <img
+  src={placeholder}
+  alt="Member"
+  className="w-24 h-24 mx-auto rounded-full mb-4 object-cover"
+/>
         <h2 className="text-xl font-semibold text-center">{name}</h2>
         <p className="text-center text-gray-400">{chapter}</p>
         <p className="text-center text-gray-400">{role}</p>
